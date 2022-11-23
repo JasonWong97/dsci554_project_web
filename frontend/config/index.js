6991e9acc10899335bd2b16e5ed43914d7d5a52e
Jiacheng Weng <jweng@usc.edu>
2022-11-22 23:07:07 -0800

    Update index.js

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -22,7 +22,8 @@ module.exports = {
 
     // Various Dev Server settings
     // host: '0.0.0.0', // can be overwritten by process.env.HOST
-    host: '127.0.0.1', // can be overwritten by process.env.HOST
+    // host: '127.0.0.1', // can be overwritten by process.env.HOST
+    host: '147.182.242.191', // can be overwritten by process.env.HOST
     port: 1200, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
